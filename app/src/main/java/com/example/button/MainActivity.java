6387ccd5b8bdc7dd48795750c2a03d194c36788e
Hasibur Rahman <139708138+HasiburRahman02@users.noreply.github.com>
2023-08-28 17:57:38 +0600

    Button & ImageButton in App Development.

---
+++ app/src/main/java/com/example/button/MainActivity.java
@@ -0,0 +1,20 @@
+/*Button & ImageButton in App Development.
+
+  Date:28 August,2023
+  Author:Hasibur Rahman(13/21|CSE-02)
+*/
+
+package com.example.button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
